a9f727a385a8d672f02eef55b8788e2ef299a890
Amrita Chanda <amrita.chanda@gmail.com>
2025-06-30 16:58:30 +0530

    Fixed: routes to quizzes; put back the authprovider and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,55 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-// import { ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import Header from './components/Header';
 import GameCardsGrid from "./games/gameCards";
 import InclusionGame from "./games/inclusionGame";
 import FinancialGame from "./games/financialGame";
-// import Login from "./Login";
-// import Admin from "./Admin";
-// import PrivateRoutes from './utils/PrivateRoutes'
-// import { AuthProvider } from './utils/AuthContext'
+import Login from "./Login";
+import Admin from "./Admin";
+import PrivateRoutes from './utils/PrivateRoutes'
+import { AuthProvider } from './utils/AuthContext'
 import './App.css'
 
-// const MainLayout = ({ children }) => {
-//   const location = useLocation();
+const MainLayout = ({ children }) => {
+  const location = useLocation();
 
-//   return (
-//     <div className="container mx-auto mt-4 w-full max-w-none">
-//       <Header currentRoute={location.pathname} />
-//       {children}
-//     </div>
-//   );
-// };
+  return (
+    <div className="container mx-auto mt-4 w-full max-w-none">
+      <Header currentRoute={location.pathname} />
+      {children}
+    </div>
+  );
+};
 
 
 function App() {
   return (
     <Router>
-      {/* <AuthProvider> */}
-      {/* <MainLayout> */}
-      <Routes>
-        <Route path="/" element={
-          < GameCardsGrid />
-        } />
-        <Route path="/inclusion" element={
-          < InclusionGame />
-        } />
-        <Route path="/financial" element={
+      <AuthProvider>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={
+              < GameCardsGrid />
+            } />
+            <Route path="/inclusion" element={
+              < InclusionGame />
+            } />
+            <Route path="/financial" element={
 
-          <FinancialGame />
+              <FinancialGame />
 
-        } />
-        <Route path="/workplace" element={
+            } />
+            <Route path="/workplace" element={
 
-          <h1>Work Etiquette Game</h1>
+              <h1>Work Etiquette Game</h1>
 
-        } />
-        <Route path="/posh" element={
+            } />
+            <Route path="/posh" element={
 
-          <h1>PoSH Game</h1>
+              <h1>PoSH Game</h1>
 
-        } />
-        {/* <Route path="/login" element={
+            } />
+            <Route path="/login" element={
 
               < Login />
 
@@ -60,11 +60,11 @@ function App() {
                 < Admin />
 
               } />
-            </Route> */}
-      </Routes>
-      {/* </MainLayout> */}
-      {/* </AuthProvider> */}
-      {/* <ToastContainer /> */}
+            </Route>
+          </Routes>
+        </MainLayout>
+      </AuthProvider>
+      <ToastContainer />
     </Router>
   )
 }

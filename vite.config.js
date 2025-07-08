2d9409acae6b2b32401af211bd523189dfd78f43
Amrita Chanda <amrita.chanda@gmail.com>
2025-07-09 00:01:10 +0530

    Fix: added redirect rule to make /dashboard work on prod

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  },
 })
